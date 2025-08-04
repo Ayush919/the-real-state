bd6f5c9a208d0d232d986dc3e1f284beb579fa26
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-08-04 12:38:58 +0530

    Update PropertyValuationForm.jsx

--- src/components/PropertyValuation/PropertyValuationForm.jsx
+++ src/components/PropertyValuation/PropertyValuationForm.jsx
@@ -34,7 +34,7 @@ export default function PropertyValuationForm() {
                 formBody.append(key, value);
             });
 
-            const res = await fetch("https://formsubmit.co/ajax/mathurayush82@gmail.com", {
+            const res = await fetch("https://formsubmit.co/ajax/${process.env.EMAIL_ID}", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
