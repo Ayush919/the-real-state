9f4c37276439af3881da75c068815b76c335cf4e
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-08-09 19:59:40 +0530

    Update page.jsx

--- src/app/listing/[id]/page.jsx
+++ src/app/listing/[id]/page.jsx
@@ -10,7 +10,7 @@ export default async function PropertyPage({params: {id}}) {
     if (!res.ok) return notFound();
     const {data: property} = await res.json();
 
-    console.log("property :: ", property)
+
     return (
         <>
             {/* HEADER SECTION */}
