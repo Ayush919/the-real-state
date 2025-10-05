35a341b197eb64f82a37f0ddd343af3937f4017d
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-10-05 21:25:58 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,10 @@ import LayoutShell from '@/components/LayoutShell'
 export const metadata = {
     title: 'Ez Grow Infra',
     description: 'Specialize in High‑End Standard Levels',
+     icons: {
+        icon: './logo.png', // Path to your favicon in the public/ directory
+        apple: '/apple-icon.png', // Path to your Apple Touch Icon
+    }
 }
 
 export default function RootLayout({ children }) {
