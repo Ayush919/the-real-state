0041126f182629fa10f3ea473554c55daf0bad22
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-10-15 22:31:03 +0530

    Integrate ToastContainer for notification support
    Added ToastContainer for notifications in the layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
 import './globals.css'
 import LayoutShell from '@/components/LayoutShell'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
     title: 'Ez Grow Infra',
     description: 'Specialize in High‑End Standard Levels',
-     icons: {
+    icons: {
         icon: './logo.png', // Path to your favicon in the public/ directory
         apple: '/apple-icon.png', // Path to your Apple Touch Icon
-    }
+    },
 }
 
 export default function RootLayout({ children }) {
@@ -15,6 +17,16 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <body>
         <LayoutShell>{children}</LayoutShell>
+        <ToastContainer
+            position="bottom-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            pauseOnHover
+            draggable
+            theme="colored"
+        />
         </body>
         </html>
     )
