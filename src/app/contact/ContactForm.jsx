2b7a6b9813ac874b85d99f0b31dec5a77a169f3c
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-10-15 22:49:42 +0530

    Update fetch URL to use NEXT_PUBLIC_EMAIL_ID

--- src/app/contact/ContactForm.jsx
+++ src/app/contact/ContactForm.jsx
@@ -27,7 +27,7 @@ export default function ContactForm() {
                 formBody.append(key, value);
             });
 
-            const res = await fetch(`https://formsubmit.co/ajax/${process.env.EMAIL_ID}`, {
+            const res = await fetch(`https://formsubmit.co/ajax/${process.env.NEXT_PUBLIC_EMAIL_ID}`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded",
