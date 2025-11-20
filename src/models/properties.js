7e700fd79e8b86879fc9686e41393a12df9138fa
Ayush Mathur <68462920+Ayush919@users.noreply.github.com>
2025-11-21 01:30:18 +0530

    Update properties.js

--- src/models/properties.js
+++ src/models/properties.js
@@ -3,19 +3,28 @@ import mongoose from "mongoose";
 
 const propertySchema = new mongoose.Schema(
     {
-        title: {type: String, required: true},
-        location: {type: String, required: true},
-        description: {type: String},
-        rooms: {type: Number},
-        bathrooms: {type: Number},
-        size: {type: Number},
-        images: [{type: String}],
-        type: {type: String, enum: ["sale", "rent"], required: true},
-        price: {type: Number},
-        retailPrice: {type: Number},
-        features: [{type: String}],
+            title: { type: String, required: true },
+            location: { type: String, required: true },
+            description: { type: String },
+
+            rooms: { type: Number },
+            bathrooms: { type: Number },
+
+            size: { type: Number },        // Built-up Area
+            superArea: { type: Number, required: true },   // Newly added
+            carpetArea: { type: Number },  // Newly added (optional)
+
+            images: [{ type: String }],
+
+            type: { type: String, enum: ["sale", "rent"], required: true },
+
+            price: { type: Number },
+            retailPrice: { type: Number },
+
+            features: [{ type: String }],
     },
-    {timestamps: true}
+    { timestamps: true }
 );
 
-export default mongoose.models.properties || mongoose.model("properties", propertySchema);
+export default mongoose.models.properties ||
+mongoose.model("properties", propertySchema);
